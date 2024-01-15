c773ed216488823c7467cf2854b46f6406aa43b9
Jonathan Samir Matthis <jonmatthis@gmail.com>
2024-01-15 17:47:28 -0500

    ignore venv

--- code/convert_docx_to_md.py
+++ code/convert_docx_to_md.py
@@ -16,11 +16,12 @@ def convert_docs_to_md(source_folder: str, output_folder: str) -> None:
     # Ensure output directory exists
     Path(output_folder).mkdir(parents=True, exist_ok=True)
 
-    for original_filepath in Path(source_folder).glob('*.docx'):
-        original_filename = original_filepath.name
+    for original_filepath in Path(source_folder).glob('*.docx'):        
+        # original_filepath = original_filepath.resolve()
+        # original_filename = original_filepath.name
         converted_filename = original_filepath.stem.replace(" ","-") + '.md'
         converted_file_path = Path(output_folder, converted_filename)
-        print(f"-------\nConverting:\n {original_filename}\n-to-\n{converted_filename}")
+        print(f"-------\nConverting:\n {original_filepath}\n-to-\n{converted_file_path}")
         # Call pandoc to convert docx to markdown
         subprocess.run(['pandoc', '-o', str(converted_file_path), str(original_filepath)])
         print("Success!\n-------")
